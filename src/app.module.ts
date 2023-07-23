d6fccdcd463cc8e4ef4de2d447d148cdd2246845
Imran <imranshkh283@gmail.com>
2023-07-23 23:59:11 +0530

    src/prisma file changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
